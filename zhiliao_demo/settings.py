bbf4f7493831578b9fe5aecfafab8f00840b5326
lilawliet <251754731@qq.com>
2018-07-06 02:04:20 +0800

    settings

--- zhiliao_demo/settings.py
+++ zhiliao_demo/settings.py
@@ -22,12 +22,20 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'u#v#jdz1w6qr!j!&hj(1g#ruv5cy=qanepq9x58u=0d4!x#hc3'
 
-# 添加域名或id地址，访问时只能通过allowed_hosts中的方式访问
-# ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['zhiliaodemo.cn', 'www.zhiliaodemo.cn', '192.168.3.64']
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
+
+############################### 开发环境 ###############################
+# ALLOWED_HOSTS = []
+# DEBUG = True
+
+########################################################################
+############################### 生产环境 ###############################
+# 添加域名或id地址，访问时只能通过allowed_hosts中的方式访问
+ALLOWED_HOSTS = ['zhiliaodemo.cn', 'www.zhiliaodemo.cn', '192.168.3.64']
+DEBUG = False
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_dist')
+########################################################################
 
 
 # Application definition
@@ -89,7 +97,7 @@ DATABASES = {
         'NAME': 'zhiliao_demo',
         'HOST': '127.0.0.1',
         'PORT': '3306',
-        'USER': 'admin',
+        'USER': 'root',
         'PASSWORD': 'root'
     }
 }
@@ -140,7 +148,6 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_dist')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'front', 'dist')
 ]
